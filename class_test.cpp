cfe73e60c9bc0631b8e0d57ac2ed065101be29fc
zach-thompson <zach.thompson@marquette.edu>
2022-02-01 14:30:49 -0600

    practice commit

---
+++ class_test.cpp
@@ -0,0 +1,7 @@
+#include "class_test.hpp"
+
+int mySpace::yourSpace::myClass::get_num_students() {
+
+    return this->num_students;
+
+}
---
+++ class_test.hpp
@@ -0,0 +1,30 @@
+/*
+* Author: Zach Thompson
+* Date: 1/31/22
+* Description: Practicing the creation and design of classes in C++
+*/
+
+#include <iostream>
+#include <string>
+
+// mySpace::yourSpace::myClass
+
+namespace myClass {
+    namespace yourspace {
+        class myClass {
+            public: // can be accessed globally
+
+                // construct myClass with a name and initial number of students, or just name
+                myClass(std::string name, int init_num_students);
+                myClass(std::string name);
+
+                int get_num_students();
+                std::string get_name();
+                void set_num_students(int num);
+
+            private: // protected items
+                int num_students;
+                std::string className;
+        };
+    }  
+}
