bf5c1cfdd5de8f6487cf18983796ae7a7e57699c
zach-thompson <zach.thompson@marquette.edu>
2022-02-18 16:52:35 -0600

    Completed miniproject0

--- MiniProjects/thompsonz_miniproject0.cpp
+++ MiniProjects/thompsonz_miniproject0.cpp
@@ -1,6 +1,44 @@
+#include "thompsonz_miniproject0.hpp"
 /*
 * Author: Zach Thompson
 * Date: 2/18/22
 * Description: Hacking time
 */
 
+hackerSpace::TimeHacking::TimeHacking(std::string objectToHack) {
+    this->objectToHack = objectToHack;
+}
+
+void hackerSpace::TimeHacking::setTimeToHack(int seconds) {
+    this->time_to_hack = seconds;
+}
+
+int hackerSpace::TimeHacking::getTimeToHack() {
+    return time_to_hack;
+}
+
+std::string hackerSpace::TimeHacking::getObjectToHack() {
+    return this->objectToHack;
+}
+
+void hackerSpace::TimeHacking::hackTime() {
+    sleep(this->time_to_hack);
+    std::cout << "Hacking complete\n";
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    std::string temp_object;
+    int temp_time;
+
+    std::cout << "What do you want to hack?\n";
+    std::cin >> temp_object;
+    std::cout << "How long do you want to hack it?\n";
+    std::cin >> temp_time;
+
+    hackerSpace::TimeHacking TimeHackings(temp_object);
+    TimeHackings.setTimeToHack(temp_time);
+
+    std::cout << "You are hacking " << TimeHackings.getObjectToHack() << " for " << TimeHackings.getTimeToHack() << " seconds\n";
+    TimeHackings.hackTime();
+}
--- MiniProjects/thompsonz_miniproject0.hpp
+++ MiniProjects/thompsonz_miniproject0.hpp
@@ -3,3 +3,30 @@
 * Date: 2/18/22
 * Description: Miniproject0 header file
 */
+
+#ifndef HACKED
+#define HACKED
+
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+namespace hackerSpace {
+    class TimeHacking {
+        public:
+
+            TimeHacking(std::string objectToHack);
+
+            void setTimeToHack(int seconds);
+            int getTimeToHack();
+            std::string getObjectToHack();
+            void hackTime();
+
+        private:
+            int time_to_hack;
+            std::string objectToHack;
+    };
+}
+
+#endif
