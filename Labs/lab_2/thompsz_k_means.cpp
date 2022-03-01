b2e772f25c34404c18ed84d087b2417cb6e145aa
zach-thompson <zach.thompson@marquette.edu>
2022-03-01 15:51:32 -0600

    function definitions

--- Labs/lab_2/thompsz_k_means.cpp
+++ Labs/lab_2/thompsz_k_means.cpp
@@ -1,6 +1,34 @@
+#include "thompsz_k_means.hpp"
 /*
 * Author: Zach Thompson
 * Date: 2/24/22
 * Description: Performs K-clustering on an input file
 * Includes: tbd
 */
+
+void kSpace::cluster::set_name(std::string in) {
+    std::string name;
+    this->name = in; 
+}
+
+std::string kSpace::cluster::get_name() {
+    return this->name;
+}
+
+void kSpace::cluster::set_mean(std::vector<float> object) {
+    float mean = 0;
+    float sum = 0;
+    float size = object.size();
+    for (int i = 0; i < size; i++) {
+        sum+= object[i];
+    }
+    this->mean = sum / size;
+}
+
+float kSpace::cluster::get_mean() {
+    return this->mean;
+}
+
+float kSpace::cluster::distance(float point) {
+    
+}
--- Labs/lab_2/thompsz_k_means.hpp
+++ Labs/lab_2/thompsz_k_means.hpp
@@ -4,3 +4,36 @@
 * Description: Perfrorms K-clustering on an input file
 * Includes: tbd
 */
+
+#ifndef k_means_guard
+#define k_means_guard
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+
+namespace kSpace{
+    class cluster{
+        public:
+            // object creation types
+            cluster(std::string name, float mean, std::vector<float> cluster_data);
+
+            // variables
+            std::string name;
+            float mean;
+            std::vector<float> cluster_data;
+
+            // functions
+            void set_name(std::string name);
+            std::string get_name();
+            void set_mean(std::vector<float> object);
+            float get_mean();
+            float distance(float point);
+
+    };
+}
+
+#endif
