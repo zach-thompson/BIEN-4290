f3b1464fc5477c1f31b973ae891e3188a70eaba5
zach-thompson <zach.thompson@marquette.edu>
2022-03-09 17:48:53 -0600

    completed midterm

--- Midterm/thompsz_makey_mcmakerson.sh
+++ Midterm/thompsz_makey_mcmakerson.sh
@@ -11,47 +11,35 @@ then
     exit 1
 fi
 
-# Save directory path in variable for clarity
-path=$1 # Use /midterm2022/Simple for testing
-
-# Create makefile to write to
+# Create local makefile to write to
+cd $1
 touch makefile
 echo -e "# Zach's Midterm makefile\n\nCC = g++ \nSHELL = /bin/bash\n" > makefile
 
-# Run through all files in directory to find file that includes "main("
+# Run through all files in directory to find ones named *.cpp
+# Run through .cpp files to find one that includes "main("
 # Save that file name under "executable"
-cd $path
+# Add file names to makefile with appropriate commands
 for FILE in *
 do
     if (echo "$FILE" | grep -q ".cpp")
     then
-        echo "$FILE: $FILE" | sed 's/.cpp/.o/' >> ~/BIEN-4290/Midterm/makefile
+        echo "$FILE: $FILE" | sed 's/.cpp/.o/' >> makefile
         
         if (grep -q "main(" $FILE)
         then
-        executable=$(echo "$FILE" | sed 's/.cpp/.o/')  >> ~/BIEN-4290/Midterm/makefile
-        echo -e "\t" '$(CC)' "-c" "$executable" '$@\n'  >> ~/BIEN-4290/Midterm/makefile
+        executable=$(echo "$FILE" | sed 's/.cpp/.o/')  >> makefile
+        echo -e "\t"'$(CC)' "-o" "$executable" '$^\n'  >> makefile
         else
-        echo -e "\t"'$(CC)' " -o " '$@\n'  >> ~/BIEN-4290/Midterm/makefile
+        echo -e "\t"'$(CC)' "-c" '$^\n'  >> makefile
         fi
     fi
 
 done
 
+# Create "all" target
+targets=$(grep ":" makefile | cut -d ":" -f1 | tr '\n' ' ') # stores lines with ":" and cuts after it, converts newline to " "
+echo "all: $targets" >> makefile
+
 # Create "clean" target
-cd ~/BIEN-4290/Midterm/
 echo -e "\nclean:\n\trm *.o" >> makefile
-
-# 1. Generate separate targets for each cpp file
-# sample_corr.o:  brennanb_corr.cpp
-#   $(CC) -o folder $^
-# sample_stats.o: brennanb_stats.cpp
-#   $(CC) -c $^
-
-# 2. Generate an "all" target which consists of the above targets and generates and executable for the code
-# all: sample_corr.o sample_stats.o
-
-# 3. Generate a "clean" target that removes all relevant compilation files
-# clean: rm *.o
-
-# 4. Once you have generated the Makefile, perform a make all, then run the executable
