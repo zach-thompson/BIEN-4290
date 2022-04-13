7d0560d470fa4428a2a75aba871475afeb2e629c
Zach Thompson <zach.thompson@marquette.edu>
2022-04-13 18:13:32 -0500

    reference issues

--- MiniProjects/MiniProject1/thompsonz_miniproject1.cpp
+++ MiniProjects/MiniProject1/thompsonz_miniproject1.cpp
@@ -1,15 +1,21 @@
-#include "../../Labs/lab_1/thompsz_stats.hpp"
+#include "/home/thompsz/BIEN-4290/Labs/lab_1/thompsz_stats.hpp"
 /*
 * Author: Zach Thompson
 * Date: 4/12/2022
 * Description: Calculate statistics of the airspeed velocity of swallows
 */
+//statSpace::statClass::statClass(std::vector<float>*) {}
 
 int main (int argc, char* argv[]) {
 
     // load file from command line argument
     // argument expected to be "swallowspeed.dat" for this project
     std::ifstream file1(argv[1]);
+
+    // check to make sure an argument was given
+    if (argv[1] == NULL) {
+        printf("No argument given");
+    }
 
     // check for file
     if (!file1.is_open()) {
@@ -26,16 +32,16 @@ int main (int argc, char* argv[]) {
     file1.close();
 
     // establish statClass to use functions
-    statSpace::statClass statClass(&data);
+    statSpace::statClass statClassy(&data);
 
     // perform calculations and print results
-    statClass.set_min(&data);
-    std::cout << "minimum: " << statClass.get_min() << "\n";
+    statClassy.set_min(&data);
+    std::cout << "minimum: " << statClassy.get_min() << "\n"; /*
     statClass.set_max(&data);
     std::cout << "maximum: " << statClass.get_max() << "\n";
     statClass.set_mean(&data);
     std::cout << "mean: " << statClass.get_mean() << "\n";
     statClass.set_stddev(&data);
     std::cout << "std_dev: " << statClass.get_stddev() << "\n";
-    
+    */
 }
