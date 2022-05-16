bac8bafa54ee1646cec33e9332fe8b753b535698
Zach Thompson <zach.thompson@marquette.edu>
2022-05-15 23:06:21 -0500

    updates

--- Labs/lab_2/thompsz_calibration.sh
+++ Labs/lab_2/thompsz_calibration.sh
@@ -18,11 +18,11 @@ do
     rm -f "clusters_${i}.txt"
     
     # preprocess the files
-    ./run "${red_}${i}.dat" "${red_}background_${i}.dat" "${green_}${i}.dat" "${green_}background_${i}.dat" "log_ratio_${i}.dat" $num_points
+    ./process "${red_}${i}.dat" "${red_}background_${i}.dat" "${green_}${i}.dat" "${green_}background_${i}.dat" "log_ratio_${i}.dat" $num_points
     cp "log_ratio_${i}.dat" log_ratio_input.dat # copy log ratio output data to new file
     # run clustering program and write results to new file
-    ./cluster >> "clusters_${i}.txt"
-    #copy the cluster output files to files who denote the corresponding time point 
+    ./cluster "log_ratio_${i}.dat" >> "clusters_${i}.txt"
+    # copy the cluster output files to files who denote the corresponding time point 
     cp expressed_gene.txt "expressed_${i}.txt"
     # write the cluster means and number of members in each cluster to a separate file for each time point
     wc -l "expressed_${i}.txt" >> "clusters_${i}.txt"
