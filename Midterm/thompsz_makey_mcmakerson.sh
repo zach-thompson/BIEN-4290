2f4acad106c385d37e9466d092f8d80be62f9488
Zach Thompson <zach.thompson@marquette.edu>
2022-05-16 17:45:12 -0500

    Completed

--- Midterm/thompsz_makey_mcmakerson.sh
+++ Midterm/thompsz_makey_mcmakerson.sh
@@ -19,6 +19,15 @@ echo -e "# Zach's midterm makefile\n\nCC = g++\nSHELL = /bin/bash\n" > makefile
 # Run through all files in directory to find ones named *.cpp
 # Run through .cpp files to find one that includes "main(", use -o
 # Add file names to makefile with appropriate commands
+
+for FILE in *
+do
+    if !(grep -q "main(" $FILE)
+    then
+    echo "$FILE" | grep ".cpp" | tr '\n' ' ' >> files.txt
+    fi
+done
+
 for FILE in *
 do
     if (grep -q "#include" "$FILE" && grep -q ".hpp" "$FILE") # identifies which files include headers
@@ -27,20 +36,20 @@ do
         included=$(grep ".hpp" $FILE | tr -d '#"' | sed 's/include //g' | tr '\n' ' ' | sed 's/.hpp/.o/g' | sed 's/'$target'//') # stores all included header files
     fi
 
+    main_includes=$(grep "." files.txt | tr '\n' ' ') # create included files variable for executable target
+
     if (echo "$FILE" | grep -q ".cpp") # looks for .cpp files
-    then        
+    then      
         if (grep -q "main(" $FILE) # determines which .cpp contains a main function
         then
-        echo -n "$FILE: $FILE " | sed 's/.cpp/.o/' >> makefile # prints target.o: target.cpp, no newline
-        echo $included >> makefile # prints included targets
-        executable=$(basename $1)
-        echo -e "\t"'$(CC)' "-o" "$executable" '$^\n'  >> makefile # prints compile line with executable (name of folder)
+            echo "$FILE: $FILE $main_includes" | sed 's/.cpp/.o/' >> makefile # prints target.o: target.cpp main_includes.cpp
+            executable=$(basename $1)
+            echo -e "\t"'$(CC)' "-o" "$executable" '$^\n'  >> makefile # prints compile line with executable (name of folder)
         else
-        echo "$FILE: $FILE $included" | sed 's/.cpp/.o/' >> makefile # prints target.o: target.cpp
-        echo -e "\t"'$(CC)' "-c" '$^\n'  >> makefile # prints compile line
+            echo "$FILE: $FILE $included" | sed 's/.cpp/.o/' >> makefile # prints target.o: target.cpp includes.cpp
+            echo -e "\t"'$(CC)' "-c" '$^\n'  >> makefile # prints compile line
         fi
     fi
-
 done
 
 # Create "all" target including all .cpp targets
@@ -48,7 +57,7 @@ targets=$(grep ":" makefile | cut -d ":" -f1 | tr '\n' ' ') # stores lines with 
 echo "all: $targets" >> makefile
 
 # Create "clean" target
-echo -e "\nclean:\n\trm *.o\n" >> makefile
+echo -e "\nclean:\n\trm *.o\n\trm *.txt" >> makefile
 
 # Once the makefile is ready, perform a make all and run the executable
 make all
